13bac40c3300e60e3a6475a7ed6e8ede281dd81c
lukasz <lukasz.stojecki@o2.pl>
2021-11-04 18:28:50 +0100

    first commit

---
+++ src/main/java/pl/stojeckilukasz/stripeinvoiceapi/StripeInvoiceApiApplication.java
@@ -0,0 +1,13 @@
+package pl.stojeckilukasz.stripeinvoiceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StripeInvoiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StripeInvoiceApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/stojeckilukasz/stripeinvoiceapi/StripeInvoiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.stojeckilukasz.stripeinvoiceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StripeInvoiceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
